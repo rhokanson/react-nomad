4a4d57a71732bfec86449390b6982e0700c28cc5
rhokanson <99600920+rhokanson@users.noreply.github.com>
2022-04-20 02:41:44 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import { useState, useEffect } from "react"
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setValue((prev) => prev +1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("I run all the time");
-  useEffect(()=>{
-    console.log("call the api")
+function Hello() {
+  useEffect(()=> {
+    console.log("created");
+    return () => console.log("destroyed");
   }, [])
-  useEffect(()=>{
-    if (keyword !== "") {
-      console.log("search for", keyword);
-    }
-  }, [keyword])
-  useEffect(()=>{
-      console.log("counter change");
-  }, [counter])
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(prev => !prev)
   return (
       <div>
-        <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-        <h1>{counter}</h1>
-        <button onClick={onClick}>click</button>
+        {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
       </div>
   );
 }
