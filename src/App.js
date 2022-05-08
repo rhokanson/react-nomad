e29c1668bf57e9193fce34490a910a438495662c
rhokanson <99600920+rhokanson@users.noreply.github.com>
2022-05-08 20:12:08 +0900

    Movie first commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
-import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes, Route, 
+}
+  from "react-router-dom"
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [coin, setCoin] = useState(['Bitcoin', '(BTC):', '34673.00017053603', 'USD']);
-  const onChange = (event) => {
-    setCoin(event.target.value.split(" "));
-  }
-  const [usd, setUsd] = useState("0");
-  const onUSD = (event) => {
-    setUsd(event.target.value)
-  }
-
-  useEffect(()=> {
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
-    .then(response => response.json())
-    .then((json)=> {
-      setCoins(json);
-      setLoading(false);
-    })
-  }, [])
-  return (
-    <div>
-     <h1>The  Coins!</h1>
-     {loading ? <strong>Loading...</strong> : <select onChange={onChange}>
-       {coins.map((coin) => 
-       <option>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</option>)}
-     </select>}
-     <hr/>
-     <label htmlFor="coin">USD</label>
-     <input id="coin"
-     type="number" 
-     value={usd}
-     onChange={onUSD}
-     /> 
-     <h2>{coin[1]} {usd / coin[2]}</h2>
-
-    </div>
-
-    );
+  return <Router>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/movie" element={<Detail />} />
+    </Routes>
+  </Router>;
 }
 
 export default App;
