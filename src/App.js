c97147ff46fbb158a73439f056c6c8906eaa1f96
rhokanson <99600920+rhokanson@users.noreply.github.com>
2022-05-08 17:46:31 +0900

    Coin converter

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value)
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos(currentArray => [toDo, ...currentArray]);
-    setToDo("");
-  };
-  console.log(toDos)
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [coin, setCoin] = useState(['Bitcoin', '(BTC):', '34673.00017053603', 'USD']);
+  const onChange = (event) => {
+    setCoin(event.target.value.split(" "));
+  }
+  const [usd, setUsd] = useState("0");
+  const onUSD = (event) => {
+    setUsd(event.target.value)
+  }
+
+  useEffect(()=> {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+    .then(response => response.json())
+    .then((json)=> {
+      setCoins(json);
+      setLoading(false);
+    })
+  }, [])
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          onChange={onChange} 
-          value={toDo} 
-          type="text" 
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-      {toDos.map((item, index) => <li key={index}>{item}</li>)}
-      </ul>
+     <h1>The  Coins!</h1>
+     {loading ? <strong>Loading...</strong> : <select onChange={onChange}>
+       {coins.map((coin) => 
+       <option>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</option>)}
+     </select>}
+     <hr/>
+     <label htmlFor="coin">USD</label>
+     <input id="coin"
+     type="number" 
+     value={usd}
+     onChange={onUSD}
+     /> 
+     <h2>{coin[1]} {usd / coin[2]}</h2>
+
     </div>
-  );
+
+    );
 }
 
 export default App;
